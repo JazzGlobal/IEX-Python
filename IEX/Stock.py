310dc9fb59380e507b15df8049099c5fbc91680c
JazzGlobal <waymer147@gmail.com>
2018-12-18 04:28:56 -0500

    Initial Commit

---
+++ IEX/Stock.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class Stock:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.book = ""
+        self.chart = ""
+
+    def build_stock(self):
+        url = "https://api.iextrading.com/1.0/stock/"
+        self.book = self.__get_book(url)
+        self.chart = self.__get_chart(url)
+
+    def __get_book(self, url):
+        response = requests.get(url + self.symbol + '/book')
+        if response.status_code == '404':
+            print(response.status_code + " Error has occurred")
+            return
+        return response.json()
+
+    def __get_chart(self,url):
+        response = requests.get(url + self.symbol + '/chart/1y')
+        if(response.status_code == '404'):
+            print(response.status_code + " Error has occurred")
+            return
+        return response.json()
