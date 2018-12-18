ec1badc3e586683655021904a4294cd1e4410e7b
user_name <waymer147@gmail.com>
2018-12-18 14:52:02 -0500

    Added '__get_dividends' function and 'dividends' key in dict.

--- IEX/Stock.py
+++ IEX/Stock.py
@@ -12,6 +12,7 @@ class Stock:
         self.data["chart"] = self.__get_chart(url)
         self.data["earnings"] = self.__get_earnings()
         self.data["company"] = self.__get_company(url)
+        self.data["dividends"] = self.__get_dividends(url)
 
     def __get_book(self, url):
         response = requests.get(url + self.symbol + '/book')
@@ -36,6 +37,13 @@ class Stock:
 
     def __get_company(self, url):
         response = requests.get(url + self.symbol + "/company")
+        if response.status_code == '404':
+            print(response.status_code + " Error has occurred")
+            return
+        return response.json()
+
+    def __get_dividends(self, url):
+        response = requests.get(url + self.symbol + "/dividends/1y")
         if response.status_code == '404':
             print(response.status_code + " Error has occurred")
             return
