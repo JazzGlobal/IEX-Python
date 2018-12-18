5078c0d724d05f1e26a1257ae353ea4aba8940e7
JazzGlobal <waymer147@gmail.com>
2018-12-18 14:36:37 -0500

    added "__get_earnings" function

--- IEX/Stock.py
+++ IEX/Stock.py
@@ -6,11 +6,13 @@ class Stock:
         self.symbol = symbol
         self.book = ""
         self.chart = ""
+        self.earnings = ""
 
     def build_stock(self):
         url = "https://api.iextrading.com/1.0/stock/"
         self.book = self.__get_book(url)
         self.chart = self.__get_chart(url)
+        self.earnings = self._get_earnings()
 
     def __get_book(self, url):
         response = requests.get(url + self.symbol + '/book')
@@ -21,6 +23,13 @@ class Stock:
 
     def __get_chart(self, url):
         response = requests.get(url + self.symbol + '/chart/1y')
+        if response.status_code == '404':
+            print(response.status_code + " Error has occurred")
+            return
+        return response.json()
+
+    def __get_earnings(self, url):
+        response = requests.get(url + self.symbol + "/earnings")
         if response.status_code == '404':
             print(response.status_code + " Error has occurred")
             return
